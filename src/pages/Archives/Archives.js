61358e699e0478d94cf0399c2d2a68b7474c0b1e
clevernyyyy <aschaal1263@gmail.com>
2022-02-24 01:53:05 -0600

    adding 2018 archive

--- src/pages/Archives/Archives.js
+++ src/pages/Archives/Archives.js
@@ -11,28 +11,33 @@ export default function Archives() {
         <div className='transparent-block'>
           <div className='archive-link-container'>
             <div className='archive-link'>
-            <a href='https://locomocosec2019.sched.com/'
-              target="_blank"
-              rel="noopener noreferrer">
-              2019: Our First Year, an Origin Story
-            </a>
-          </div>
+              <a href='https://locomocosecurityconference2018.sched.com/'
+                target="_blank"
+                rel="noopener noreferrer">
+                2018: Our First Year, an Origin Story
+              </a>
+            </div>
           </div>
           <div className='archive-link-container'>
             <div className='archive-link'>
-            <a href='https://sessionize.com/api/v2/v9cagsg8/view/GridSmart?preview=True'
-              target="_blank"
-              rel="noopener noreferrer">
-              2020: LocoMocoSec (Virtual)
-            </a>
+              <a href='https://locomocosec2019.sched.com/'
+                target="_blank"
+                rel="noopener noreferrer">
+                2019: Year Two, Bigger and Better
+              </a>
+            </div>
           </div>
-            
-            
+          <div className='archive-link-container'>
+            <div className='archive-link'>
+              <a href='https://sessionize.com/api/v2/v9cagsg8/view/GridSmart?preview=True'
+                target="_blank"
+                rel="noopener noreferrer">
+                2020: LocoMocoSec (Virtual)
+              </a>
+            </div>
+          </div>
         </div>
-
       </div>
-    
-    </div>
     </div>
 
   );
