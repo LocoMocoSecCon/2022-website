0134a21773a13b2a8221382a661034a45e2b9fd2
clevernyyyy <aschaal1263@gmail.com>
2022-03-07 02:00:49 -0600

    adding two training classes

--- src/pages/Training/Training.js
+++ src/pages/Training/Training.js
@@ -4,6 +4,9 @@ import './Training.css';
 import jim from '../../static/jim.png';
 import kostas from '../../static/kostas.png';
 import sven from '../../static/sven.png';
+import philippe from '../../static/philippe.jpeg';
+import seth from '../../static/seth.jpeg';
+import ken from '../../static/ken.jpeg';
 
 export default function Training() {
   return (
@@ -13,22 +16,6 @@ export default function Training() {
         <h1 className='sub-page-title'>Training</h1>
 
         <div className='transparent-block'>
-          <div className='transparent-block-container'>
-            <div className='transparent-block-title'>WEB & API SECURITY WORKSHOP</div>
-            <div className='transparent-block-sub-title'>2 day workshop  ·  June 27-28<sup>th</sup></div>
-            <div className='transparent-block-text'>This secure coding training class is designed to benefit any web developer, architect, security professional or other software development professional who needs to build and maintain secure web and web service software. Classes are custom built from learning modules including Client Security, Identity and Access Management, and SDLC, AppSec, and DevOps Processes.</div>
-          
-            <div className='trainer-block'>
-              <div className='trainer-instructor'>Instructor: Jim Manico</div>
-
-              <div className='trainer-information'>
-                <img src={jim} className='trainer-pic' alt='Jim Manico'/>
-                <p className='trainer-para'>Jim Manico is the founder of Manicode Security where he trains software developers on secure coding and security engineering. He is also the co-founder of the LocoMoco Security Conference and is a investor/advisor for BitDiscovery and Signal Sciences. Jim is a frequent speaker on secure software practices and is a member of the JavaOne rockstar speaker community. He is the author of “Iron-Clad Java: Building Secure Web Applications” from McGraw-Hill.
-                </p>
-              </div>
-            </div>
-          
-          </div>
 
           <div className='transparent-block-container'>
             <div className='transparent-block-title'>AZURE & AWS SECURITY WORKSHOP</div>
@@ -47,6 +34,25 @@ export default function Training() {
           
           </div>
 
+
+          <div className='transparent-block-container'>
+            <div className='transparent-block-title'>BECOME AN API SECURITY CHAMPION WORKSHOP</div>
+            <div className='transparent-block-sub-title'>2 day workshop  ·  June 27-28<sup>th</sup></div>
+            <div className='transparent-block-text'>Building secure APIs and microservices is hard, really hard. Not only do you have to make the right architectural security decisions, you also have to be aware of various implementation vulnerabilities to ensure the security of your applications. This workshop provides API developers with the necessary knowledge to assess and improve the security of their APIs. In this workshop, you will discover best practices for building secure APIs. We investigate various techniques to implement authentication and authorization, along with their trade-offs and pitfalls. We dive deep into handling JSON Web Tokens, along with current best practices for securing an API with OAuth 2.0. Additionally, we complement primary defenses with defense-in-depth strategies to enhance the security of your APIs. This course offers practical and immediately applicable security advice for architects and developers. Throughout the course, Philippe is available to answer any questions, including concrete scenarios applying to your own applications.</div>
+          
+            <div className='trainer-block'>
+              <div className='trainer-instructor'>Instructor: Dr. Philippe De Ryck</div>
+
+              <div className='trainer-information'>
+                <img src={philippe} className='trainer-pic' alt='Dr. Philippe De Ryck'/>
+                <p className='trainer-para'>Philippe De Ryck helps developers protect companies through better web security. His Ph.D. in web security from KU Leuven lies at the basis of his exceptional knowledge of the security landscape. As the founder of Pragmatic Web Security, Philippe delivers security training and security consulting to companies worldwide. His online course platform allows anyone to learn complex security topics at their own pace. Philippe is a Google Developer Expert and an Auth0 Ambassador for his community contributions on the security of web applications and APIs.
+                </p>
+              </div>
+            </div>
+          
+          </div>
+
+
           <div className='transparent-block-container'>
             <div className='transparent-block-title'>iOS & ANDROID MOBILE SECURITY WORKSHOP</div>
             <div className='transparent-block-sub-title'>2 day workshop  ·  June 27-28<sup>th</sup></div>
@@ -64,6 +70,48 @@ export default function Training() {
           
           </div>
 
+
+          <div className='transparent-block-container'>
+            <div className='transparent-block-title'>PRACTICAL SECURITY CODE REVIEW WORKSHOP</div>
+            <div className='transparent-block-sub-title'>2 day workshop  ·  June 27-28<sup>th</sup></div>
+            <div className='transparent-block-text'>Ready to take your bug hunting to a deeper level? Ever been tasked with reviewing source code for SQL Injection, XSS, Access Control and other security flaws? Does the idea of reviewing this code leave you with heartburn? Join us for Practical Secure Code Review, aka Seth & Ken’s Excellent Adventures (in Secure Code Review). This course introduces a proven methodology and framework for performing a secure code review, as well as addressing common challenges in modern secure code review. Short circuit developing individual secure code review techniques by gleaning from Seth & Ken's past adventures in performing hundreds of code reviews and the lessons we’ve learned along the way. We will share a proven methodology to perform security analysis of any source code repository and suss out security flaws, no matter the size of the code base, or the framework, or the language.</div>
+          
+            <div className='trainer-block'>
+              <div className='trainer-instructor'>Instructor: Seth Law</div>
+              <div className='trainer-information'>
+                <img src={seth} className='trainer-pic' alt='Seth Law'/>
+                <p className='trainer-para'>Seth Law is an experienced Application Security Professional with over 15 years of experience in the computer security industry. During this time, Seth has worked within multiple disciplines in the security field, from software development to network protection, both as a manager and individual contributor. Seth has honed his application security skills using offensive and defensive techniques, including tool development. Seth is employed as a security consultant, hosts the Absolute AppSec podcast with Ken Johnson, and is a regular speaker at developer meetups and security events, including Blackhat, Defcon, CactusCon, and other regional conferences.
+                </p>
+              </div>
+
+              <div className='trainer-instructor'>Instructor: Ken Johnson</div>
+              <div className='trainer-information'>
+                <img src={ken} className='trainer-pic' alt='Ken Johnson'/>
+                <p className='trainer-para'>Ken Johnson, has been hacking web applications professionally for 12 years and given security training for 9 of those years. Ken is both a breaker and builder and currently works on the GitHub application security team. Previously, Ken has spoken at RSA, You Sh0t the Sheriff, Insomnihack, CERN,  DerbyCon, AppSec USA, AppSec DC, AppSec California, DevOpsDays DC, LASCON, RubyNation, and numerous Ruby, OWASP, and AWS events about appsec, devops security, and AWS security. Ken's current projects are WeirdAAL, OWASP Railsgoat, and the Absolute AppSec podcast with Seth Law.
+                </p>
+              </div>
+            </div>
+          
+          </div>
+
+
+
+          <div className='transparent-block-container'>
+            <div className='transparent-block-title'>WEB & API SECURITY WORKSHOP</div>
+            <div className='transparent-block-sub-title'>2 day workshop  ·  June 27-28<sup>th</sup></div>
+            <div className='transparent-block-text'>This secure coding training class is designed to benefit any web developer, architect, security professional or other software development professional who needs to build and maintain secure web and web service software. Classes are custom built from learning modules including Client Security, Identity and Access Management, and SDLC, AppSec, and DevOps Processes.</div>
+          
+            <div className='trainer-block'>
+              <div className='trainer-instructor'>Instructor: Jim Manico</div>
+
+              <div className='trainer-information'>
+                <img src={jim} className='trainer-pic' alt='Jim Manico'/>
+                <p className='trainer-para'>Jim Manico is the founder of Manicode Security where he trains software developers on secure coding and security engineering. He is also the co-founder of the LocoMoco Security Conference and is a investor/advisor for BitDiscovery and Signal Sciences. Jim is a frequent speaker on secure software practices and is a member of the JavaOne rockstar speaker community. He is the author of “Iron-Clad Java: Building Secure Web Applications” from McGraw-Hill.
+                </p>
+              </div>
+            </div>
+          
+          </div>
 
         </div>
 
